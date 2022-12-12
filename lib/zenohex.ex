7dfe05595f7cbfc7e9356484f477655f60afb79f
s-hosoai <shintaro.hosoai@gmail.com>
2022-12-12 18:06:40 +0900

    create project with mix.new

---
+++ lib/zenohex.ex
@@ -0,0 +1,18 @@
+defmodule Zenohex do
+  @moduledoc """
+  Documentation for `Zenohex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zenohex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zenohex_test.exs
@@ -0,0 +1,8 @@
+defmodule ZenohexTest do
+  use ExUnit.Case
+  doctest Zenohex
+
+  test "greets the world" do
+    assert Zenohex.hello() == :world
+  end
+end
