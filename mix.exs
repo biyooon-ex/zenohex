bb7604e171bc33206326461363157d0a0647c2d6
s-hosoai <shintaro.hosoai@gmail.com>
2023-02-25 21:31:00 +0900

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Zenohex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~>0.26.0"}
+      {:rustler, "~>0.26.0"},
+      {:ex_doc, "~> 0.10", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
