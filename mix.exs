7b2f74d722e4055160de0a393977a4b2dbf96eb5
s-hosoai <shintaro.hosoai@gmail.com>
2023-09-29 16:24:05 +0900

    update elixir libraries

--- mix.exs
+++ mix.exs
@@ -24,12 +24,9 @@ defmodule Zenohex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler_precompiled, "~> 0.6"},
-      {:rustler, ">= 0.0.0", optional: true},
-      {:ex_doc, "~> 0.10", only: :dev}
-
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler_precompiled, "~> 0.7.0"},
+      {:rustler, ">= 0.29.1", optional: true},
+      {:ex_doc, "~> 0.30.6", only: :dev}
     ]
   end
 
