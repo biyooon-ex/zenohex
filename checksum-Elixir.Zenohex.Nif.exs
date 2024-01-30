737ae553b0a21d2609af1d42a8cd5913580d6cb6
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-01-30 15:46:54 +0900

    Add checksum-Elixir.Zenohex.Nif.exs for v0.2.0-rc.1

---
+++ checksum-Elixir.Zenohex.Nif.exs
@@ -0,0 +1,11 @@
+%{
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7877bc6913c252485ed10bbd6e4081b8f74be84dd8f251e3bf40b767871ea3da",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9353f1943337976731ab1111f3df524510fc990217a39989714dc0b56a813880",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:3ab203d686436f0d935907dd80fe035890c3f692fc94b1a8d03536d88f9a6f61",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:b1406f2bc2cda6df768432c700d1bb571dd6ca2b85584af4cc88d17dd4cbaa96",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:4fdcaac50d65e6d069fde911b25e30c04aff4c8c8fd69917906a870db47814e3",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:32bc5b176bb81b384265f061e4953fd0b265dc6cc2219897962689059a1f8c6e",
+  "libzenohex_nif-v0.2.0-rc.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:0c306e0b3a4cb790a7f99037c2925ea76efebf254ab7d9a238820097c68b95ce",
+  "zenohex_nif-v0.2.0-rc.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:32d156d75f8815338dfd0ff0664eef43030b5469c505fd741e14b1d2b9c14eca",
+  "zenohex_nif-v0.2.0-rc.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:81c7fec9f38006f74843230ae82d9a6895912e7b1353261e06c781ea9f87174b",
+}
