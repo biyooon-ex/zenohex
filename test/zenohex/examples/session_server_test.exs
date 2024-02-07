88b5463a774abaf4a1beae14e3ad5c02df498c6e
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-07 15:35:46 +0900

    Modify examples/session_server's test

--- test/zenohex/examples/session_server_test.exs
+++ test/zenohex/examples/session_server_test.exs
@@ -2,6 +2,7 @@ defmodule Zenohex.Examples.SessionServerTest do
   use ExUnit.Case
 
   alias Zenohex.Examples.SessionServer
+  alias Zenohex.Examples.SubscriberServer
   alias Zenohex.Examples.QueryableServer
   alias Zenohex.Query
   alias Zenohex.Sample
@@ -15,8 +16,40 @@ defmodule Zenohex.Examples.SessionServerTest do
     assert is_reference(SessionServer.session())
   end
 
-  test "put/2" do
-    assert SessionServer.put("key/expression", "value") == :ok
+  describe "put/2" do
+    test "with subscriber" do
+      me = self()
+
+      start_supervised!(
+        {SubscriberServer,
+         %{
+           session: SessionServer.session(),
+           key_expr: "key/expression/**",
+           callback: fn sample -> send(me, sample) end
+         }}
+      )
+
+      assert SessionServer.put("key/expression/put", "value") == :ok
+      assert_receive %Sample{key_expr: "key/expression/put", value: "value"}
+    end
+  end
+
+  describe "delete/1" do
+    test "with subscriber" do
+      me = self()
+
+      start_supervised!(
+        {SubscriberServer,
+         %{
+           session: SessionServer.session(),
+           key_expr: "key/expression/**",
+           callback: fn sample -> send(me, sample) end
+         }}
+      )
+
+      assert SessionServer.delete("key/expression/delete") == :ok
+      assert_receive %Sample{key_expr: "key/expression/delete", kind: :delete}
+    end
   end
 
   describe "get/2" do
