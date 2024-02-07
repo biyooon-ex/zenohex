cce46712f36b72ed92906e9dd4cda4c29a765d8e
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-07 15:08:37 +0900

    Add subscriber_server to examples

---
+++ lib/zenohex/examples/subscriber_server.ex
@@ -0,0 +1,42 @@
+defmodule Zenohex.Examples.SubscriberServer do
+  use GenServer
+
+  require Logger
+
+  alias Zenohex.Session
+  alias Zenohex.Subscriber
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(args) do
+    session = Map.fetch!(args, :session)
+    key_expr = Map.fetch!(args, :key_expr)
+    callback = Map.fetch!(args, :callback)
+
+    {:ok, subscriber} = Session.declare_subscriber(session, key_expr)
+    send(self(), :loop)
+
+    {:ok, %{subscriber: subscriber, callback: callback}}
+  end
+
+  def handle_info(:loop, state) do
+    case Subscriber.recv_timeout(state.subscriber, 1000) do
+      {:ok, sample} ->
+        state.callback.(sample)
+        send(self(), :loop)
+
+      {:error, :timeout} ->
+        send(self(), :loop)
+
+      {:error, :disconnected} ->
+        raise("unreachable!")
+
+      {:error, error} ->
+        Logger.error(inspect(error))
+    end
+
+    {:noreply, state}
+  end
+end
---
+++ test/zenohex/examples/subscriber_server_test.exs
@@ -0,0 +1,23 @@
+defmodule Zenohex.Examples.SubscriberServerTest do
+  use ExUnit.Case
+
+  alias Zenohex.Examples.SubscriberServer
+  alias Zenohex.Session
+  alias Zenohex.Sample
+
+  test "start_link/1" do
+    {:ok, session} = Zenohex.open()
+    key_expr = "key/expression/**"
+
+    me = self()
+    callback = fn sample -> send(me, sample) end
+
+    start_supervised!(
+      {SubscriberServer, %{session: session, key_expr: key_expr, callback: callback}}
+    )
+
+    Session.put(session, "key/expression/put", "put")
+
+    assert_receive(%Sample{key_expr: "key/expression/put", value: "put"})
+  end
+end
