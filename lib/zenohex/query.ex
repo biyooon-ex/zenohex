632f1922ea8f738203f08e7fa11cefa44bbafdfe
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-08 12:06:17 +0900

    Add docs to Query functions

--- lib/zenohex/query.ex
+++ lib/zenohex/query.ex
@@ -25,11 +25,23 @@ defmodule Zenohex.Query do
     defstruct target: :best_matching, consolidation: :auto
   end
 
+  @doc ~S"""
+  Sends a reply to this Query. User can call `reply/2` multiple times to send multiple samples.
+
+  > ### Warning {: .warning}
+  > Do not forget to call `finish_reply/1` to finish the reply.
+  """
   @spec reply(t(), Sample.t()) :: :ok | {:error, reason :: any()}
   def reply(query, sample) when is_struct(query, __MODULE__) and is_struct(sample, Sample) do
     Nif.query_reply(query, sample)
   end
 
+  @doc ~S"""
+  Finish reply.
+
+  > ### Warning {: .warning}
+  > `finish_reply/1` must be called after `reply/2`.
+  """
   @spec finish_reply(t()) :: :ok | {:error, reason :: any()}
   def finish_reply(query) when is_struct(query, __MODULE__) do
     Nif.query_finish_reply(query)
