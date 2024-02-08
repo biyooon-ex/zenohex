b51f312a7e143fb5be356d84fcf096a940252824
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-08 13:49:12 +0900

    Refactor Zenohex.Nif

--- lib/zenohex/nif.ex
+++ lib/zenohex/nif.ex
@@ -22,91 +22,51 @@ defmodule Zenohex.Nif do
   alias Zenohex.Queryable
   alias Zenohex.Query
 
-  def zenoh_open() do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  defp err(), do: :erlang.nif_error(:nif_not_loaded)
 
-  def zenoh_scouting_delay_zero_session() do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def zenoh_open(), do: err()
+
+  def zenoh_scouting_delay_zero_session(), do: err()
 
   for type <- ["integer", "float", "binary"] do
-    def unquote(:"session_put_#{type}")(_session, _key_expr, _value) do
-      :erlang.nif_error(:nif_not_loaded)
-    end
+    def unquote(:"session_put_#{type}")(_session, _key_expr, _value), do: err()
   end
 
-  def session_get_reply_receiver(_session, _selector, _opts \\ %Query.Options{}) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def session_get_reply_receiver(_session, _selector, _opts \\ %Query.Options{}), do: err()
 
-  def session_get_reply_timeout(_receiver, _timeout_us) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def session_get_reply_timeout(_receiver, _timeout_us), do: err()
 
-  def session_delete(_session, _key_expr) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def session_delete(_session, _key_expr), do: err()
 
-  def declare_publisher(_session, _key_expr, _opts \\ %Publisher.Options{}) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def declare_publisher(_session, _key_expr, _opts \\ %Publisher.Options{}), do: err()
 
   for type <- ["integer", "float", "binary"] do
-    def unquote(:"publisher_put_#{type}")(_publisher, _value) do
-      :erlang.nif_error(:nif_not_loaded)
-    end
+    def unquote(:"publisher_put_#{type}")(_publisher, _value), do: err()
   end
 
-  def publisher_delete(_publisher) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def publisher_delete(_publisher), do: err()
 
-  def publisher_congestion_control(_publisher, _congestion_control) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def publisher_congestion_control(_publisher, _congestion_control), do: err()
 
-  def publisher_priority(_publisher, _priority) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def publisher_priority(_publisher, _priority), do: err()
 
-  def declare_subscriber(_session, _key_expr, _opts \\ %Subscriber.Options{}) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def declare_subscriber(_session, _key_expr, _opts \\ %Subscriber.Options{}), do: err()
 
-  def subscriber_recv_timeout(_subscriber, _timeout_us) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def subscriber_recv_timeout(_subscriber, _timeout_us), do: err()
 
-  def declare_pull_subscriber(_session, _key_expr, _opts \\ %Subscriber.Options{}) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def declare_pull_subscriber(_session, _key_expr, _opts \\ %Subscriber.Options{}), do: err()
 
-  def pull_subscriber_pull(_pull_subscriber) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def pull_subscriber_pull(_pull_subscriber), do: err()
 
-  def pull_subscriber_recv_timeout(_pull_subscriber, _timeout_us) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def pull_subscriber_recv_timeout(_pull_subscriber, _timeout_us), do: err()
 
-  def declare_queryable(_session, _key_expr, _opts \\ %Queryable.Options{}) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def declare_queryable(_session, _key_expr, _opts \\ %Queryable.Options{}), do: err()
 
-  def queryable_recv_timeout(_queryable, _timeout_us) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def queryable_recv_timeout(_queryable, _timeout_us), do: err()
 
-  def query_reply(_query, _sample) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def query_reply(_query, _sample), do: err()
 
-  def query_finish_reply(_query) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def query_finish_reply(_query), do: err()
 
-  def key_expr_intersects(_l, _r) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
+  def key_expr_intersects(_key_expr1, _key_expr2), do: err()
 end
