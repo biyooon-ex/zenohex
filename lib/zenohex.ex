a480ed8839d7ebc5b083181961affc7c7595903a
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-16 11:06:59 +0900

    Modify Zenohex.open/1

--- lib/zenohex.ex
+++ lib/zenohex.ex
@@ -17,10 +17,10 @@ defmodule Zenohex do
   """
   @spec open(Config.t()) :: {:ok, Session.t()} | {:error, reason :: any()}
   def open(config \\ %Config{}) do
-    if delay = System.get_env("SCOUTING_DELAY") do
-      Nif.zenoh_open(%Config{scouting: %Scouting{delay: String.to_integer(delay)}})
-    else
+    if is_nil(delay = System.get_env("SCOUTING_DELAY")) do
       Nif.zenoh_open(config)
+    else
+      Nif.zenoh_open(%Config{config | scouting: %Scouting{delay: String.to_integer(delay)}})
     end
   end
 
