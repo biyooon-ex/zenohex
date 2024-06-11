13977d5d1d05938e00d07cfd9d60ab29ec3e27b2
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-06-11 18:19:01 +0900

    change source_url to new organization

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Zenohex.MixProject do
   use Mix.Project
 
   @version "0.2.0"
-  @source_url "https://github.com/b5g-ex/zenohex"
+  @source_url "https://github.com/biyooon-ex/zenohex"
 
   def project do
     [
