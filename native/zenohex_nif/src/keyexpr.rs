1192fd3aaf9d8aaba8bdadaf86c14ae61bc6cdce
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-11-10 13:38:46 +0900

    Able to compile keyexpr.rs

--- native/zenohex_nif/src/keyexpr.rs
+++ native/zenohex_nif/src/keyexpr.rs
@@ -1,8 +1,8 @@
 use zenoh::key_expr::{keyexpr, KeyExpr};
 
 #[rustler::nif]
-fn key_expr_intersects(key_expr1: &str, key_expr2: &str) -> bool {
-    let key_expr1 = unsafe { KeyExpr::from_str_uncheckend(key_expr1) };
-    let key_expr2 = unsafe { KeyExpr::from_str_uncheckend(key_expr2) };
+fn key_expr_intersects(key_expr1: String, key_expr2: String) -> bool {
+    let key_expr1 = unsafe { KeyExpr::from_string_unchecked(key_expr1) };
+    let key_expr2 = unsafe { KeyExpr::from_string_unchecked(key_expr2) };
     keyexpr::intersects(&key_expr1, &key_expr2)
 }
