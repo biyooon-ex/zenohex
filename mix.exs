6ea40bb54ae5e2caa2837daba403fac35fa4992f
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-04-24 10:47:51 +0900

    mix deps.update --all

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Zenohex.MixProject do
   defp deps do
     [
       {:rustler_precompiled, "~> 0.8.2"},
-      {:rustler, "== 0.34.0", optional: true},
+      {:rustler, "== 0.36.1", optional: true},
       {:ex_doc, "~> 0.33", only: :dev},
       {:mix_test_watch, "~> 1.2", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
