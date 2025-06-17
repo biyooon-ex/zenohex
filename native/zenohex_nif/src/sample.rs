6f94490c803286ce4a4093b736f667e41b1dc368
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-06-17 21:25:53 +0900

    Refactor sample.rs

--- native/zenohex_nif/src/sample.rs
+++ native/zenohex_nif/src/sample.rs
@@ -12,10 +12,12 @@ impl<'a> ZenohexSample<'a> {
     pub(crate) fn from(env: rustler::Env<'a>, sample: &zenoh::sample::Sample) -> Self {
         let payload = sample.payload();
         let mut payload_binary = rustler::OwnedBinary::new(payload.len()).unwrap();
+
         payload_binary
             .as_mut_slice()
             .write_all(&payload.to_bytes())
             .unwrap();
+
         ZenohexSample {
             key_expr: sample.key_expr().to_string(),
             payload: payload_binary.release(env),
