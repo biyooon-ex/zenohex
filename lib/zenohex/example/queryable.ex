f5efc13c2929aee519d57704c00190a0b5d8f1ff
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-06-17 19:56:29 +0900

    Add Zenohex.Example.Queryable

---
+++ lib/zenohex/example/queryable.ex
@@ -0,0 +1,41 @@
+defmodule Zenohex.Example.Queryable do
+  use GenServer
+
+  require Logger
+
+  def start_link(args) do
+    name = Keyword.get(args, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, args, name: name)
+  end
+
+  def init(args) do
+    session_id =
+      Keyword.get(args, :session_id) ||
+        Zenohex.Session.open() |> then(fn {:ok, session_id} -> session_id end)
+
+    key_expr = Keyword.get(args, :key_expr, "key/expr")
+
+    callback = Keyword.get(args, :callback, &Logger.debug("#{inspect(&1)}"))
+
+    callback_payload = Keyword.get(args, :callback_payload, &inspect(&1))
+
+    {:ok, queryable_id} = Zenohex.Session.declare_queryable(session_id, key_expr)
+
+    {:ok,
+     %{
+       queryable_id: queryable_id,
+       key_expr: key_expr,
+       callback: callback,
+       callback_payload: callback_payload
+     }}
+  end
+
+  def handle_info(%Zenohex.Query{} = query, state) do
+    %{callback: callback, callback_payload: callback_payload} = state
+
+    callback.(query)
+    :ok = Zenohex.Query.reply(%{query | payload: callback_payload.(query)})
+
+    {:noreply, state}
+  end
+end
---
+++ test/zenohex/example/queryable_test.exs
@@ -0,0 +1,21 @@
+defmodule Zenohex.Example.QueryableTest do
+  use ExUnit.Case
+
+  test "reply correctly" do
+    me = self()
+
+    {:ok, _pid} =
+      start_supervised(
+        {Zenohex.Example.Queryable,
+         [
+           key_expr: "key/expr",
+           callback: fn query -> send(me, query) end
+         ]},
+        restart: :temporary
+      )
+
+    {:ok, %Zenohex.Sample{}} = Zenohex.Session.get("key/expr", 100)
+
+    assert_receive %Zenohex.Query{}
+  end
+end
