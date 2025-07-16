11470c08a0d1b30e5070c3b3975eef9a11629bef
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-07-17 08:43:15 +0900

    Update Zenohex.Examples.Publisher and its test

--- lib/zenohex/examples/publisher.ex
+++ lib/zenohex/examples/publisher.ex
@@ -1,33 +1,74 @@
 defmodule Zenohex.Examples.Publisher do
-  @moduledoc false
+  @moduledoc """
+  Example `GenServer` implementation of `Publisher`
+  using `Zenohex.Session.declare_publisher/3`.
+
+  This example demonstrates how to publish.
+
+  For the actual implementation, please refer to the following,
+
+  - #{Zenohex.MixProject.project()[:source_url]}/tree/main/#{Path.relative_to_cwd(__ENV__.file)}
+
+  ## Examples
+
+      iex> Zenohex.Examples.Publisher.start_link([])
+  """
 
   use GenServer
 
-  def put(payload) do
-    put(__MODULE__, payload)
-  end
-
-  def put(name, payload) do
+  @doc """
+  Sends a `Zenohex.Sample` with `kind: :put` and the given payload.
+  """
+  @spec put(module(), binary()) :: :ok | {:error, term()}
+  def put(name \\ __MODULE__, payload) do
     GenServer.call(name, {:put, payload})
   end
 
-  def delete() do
-    delete(__MODULE__)
-  end
-
-  def delete(name) do
+  @doc """
+  Sends a `Zenohex.Sample` with `kind: :delete`.
+  """
+  @spec delete(module()) :: :ok | {:error, term()}
+  def delete(name \\ __MODULE__) do
     GenServer.call(name, :delete)
   end
 
+  @doc """
+  Starts #{__MODULE__}.
+
+  ## Parameters
+
+    * `args` – a keyword list that can include the following keys:
+      * `:session_id` – the ID of the session
+      * `:key_expr` – the key expression to subscribe to
+  """
+  @spec start_link([
+          {:session_id, Zenohex.Session.id()}
+          | {:key_expr, String.t()}
+        ]) :: GenServer.on_start()
   def start_link(args) do
     name = Keyword.get(args, :name, __MODULE__)
     GenServer.start_link(__MODULE__, args, name: name)
   end
 
+  @doc """
+  Stops #{__MODULE__}
+  """
+  @spec stop(module()) :: :ok
+
+  def stop(name \\ __MODULE__) do
+    GenServer.call(name, :stop)
+  end
+
+  @doc false
+  def child_spec(init_arg), do: super(init_arg)
+
+  @doc false
   def init(args) do
     session_id =
-      Keyword.get(args, :session_id) ||
-        Zenohex.Session.open() |> then(fn {:ok, session_id} -> session_id end)
+      Keyword.get_lazy(args, :session_id, fn ->
+        {:ok, session_id} = Zenohex.Session.open()
+        session_id
+      end)
 
     key_expr = Keyword.get(args, :key_expr, "key/expr")
 
@@ -40,14 +81,21 @@ defmodule Zenohex.Examples.Publisher do
   end
 
   def handle_call({:put, payload}, _from, state) do
-    %{publisher_id: publisher_id} = state
-    reply = Zenohex.Publisher.put(publisher_id, payload)
+    reply = Zenohex.Publisher.put(state.publisher_id, payload)
+
     {:reply, reply, state}
   end
 
   def handle_call(:delete, _from, state) do
-    %{publisher_id: publisher_id} = state
-    reply = Zenohex.Publisher.delete(publisher_id)
+    reply = Zenohex.Publisher.delete(state.publisher_id)
+
     {:reply, reply, state}
+  end
+
+  def handle_call(:stop, _from, state) do
+    reply = Zenohex.Publisher.undeclare(state.publisher_id)
+    reason = :normal
+
+    {:stop, reason, reply, state}
   end
 end
--- test/zenohex/examples/publisher_test.exs
+++ test/zenohex/examples/publisher_test.exs
@@ -7,35 +7,39 @@ defmodule Zenohex.Examples.PublisherTest do
       |> Zenohex.Test.Support.TestHelper.scouting_delay(0)
       |> Zenohex.Session.open()
 
-    %{session_id: session_id}
+    on_exit(fn -> Zenohex.Session.close(session_id) end)
+
+    key_expr = "key/expr"
+
+    %{me: self(), session_id: session_id, key_expr: key_expr}
   end
 
-  test "put correctly", context do
-    me = self()
-
+  test "example works correctly", context do
     {:ok, _pid} =
       start_supervised(
         {Zenohex.Examples.Subscriber,
          [
            session_id: context.session_id,
-           key_expr: "key/expr",
-           callback: fn sample -> send(me, sample) end
+           key_expr: context.key_expr,
+           callback: fn sample -> send(context.me, sample) end
          ]},
         restart: :temporary
       )
 
-    {:ok, _pid} =
-      start_supervised(
-        {Zenohex.Examples.Publisher,
-         [
-           session_id: context.session_id,
-           key_expr: "key/expr"
-         ]},
-        restart: :temporary
-      )
+    assert {:ok, _pid} =
+             Zenohex.Examples.Publisher.start_link(
+               session_id: context.session_id,
+               key_expr: context.key_expr
+             )
 
-    :ok = Zenohex.Examples.Publisher.put("payload")
+    assert :ok = Zenohex.Examples.Publisher.put("payload")
 
-    assert_receive %Zenohex.Sample{}
+    assert_receive %Zenohex.Sample{kind: :put, payload: "payload"}
+
+    assert :ok = Zenohex.Examples.Publisher.delete()
+
+    assert_receive %Zenohex.Sample{kind: :delete}
+
+    assert :ok = Zenohex.Examples.Publisher.stop()
   end
 end
