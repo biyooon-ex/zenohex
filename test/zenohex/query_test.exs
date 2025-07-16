49db70c4c82f03dd5e48ed0237ca8b69c8a07417
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-07-16 09:17:44 +0900

    Add docs and tests to Zenohex.Query

---
+++ test/zenohex/query_test.exs
@@ -0,0 +1,65 @@
+defmodule Zenohex.QueryTest do
+  use ExUnit.Case
+
+  setup do
+    {:ok, session_id} =
+      Zenohex.Config.default()
+      |> Zenohex.Test.Support.TestHelper.scouting_delay(0)
+      |> Zenohex.Session.open()
+
+    on_exit(fn -> Zenohex.Session.close(session_id) end)
+
+    {:ok, queryable_id} = Zenohex.Session.declare_queryable(session_id, "key/expr", self())
+
+    %{
+      session_id: session_id,
+      queryable_id: queryable_id
+    }
+  end
+
+  test "reply/3", context do
+    task =
+      Task.async(Zenohex.Session, :get, [
+        context.session_id,
+        "key/expr/**",
+        100
+      ])
+
+    assert_receive %Zenohex.Query{zenoh_query: zenoh_query}
+
+    assert :ok = Zenohex.Query.reply(zenoh_query, "key/expr/1", "payload")
+
+    assert {:ok, [%Zenohex.Sample{kind: :put, key_expr: "key/expr/1", payload: "payload"}]} =
+             Task.await(task)
+  end
+
+  test "reply_error/3", context do
+    task =
+      Task.async(Zenohex.Session, :get, [
+        context.session_id,
+        "key/expr/**",
+        100
+      ])
+
+    assert_receive %Zenohex.Query{zenoh_query: zenoh_query}
+
+    assert :ok = Zenohex.Query.reply_error(zenoh_query, "payload")
+
+    assert {:ok, [%Zenohex.Query.ReplyError{payload: "payload"}]} = Task.await(task)
+  end
+
+  test "reply_delete/3", context do
+    task =
+      Task.async(Zenohex.Session, :get, [
+        context.session_id,
+        "key/expr/**",
+        100
+      ])
+
+    assert_receive %Zenohex.Query{zenoh_query: zenoh_query}
+
+    assert :ok = Zenohex.Query.reply_delete(zenoh_query, "key/expr/1")
+
+    assert {:ok, [%Zenohex.Sample{kind: :delete, key_expr: "key/expr/1"}]} = Task.await(task)
+  end
+end
