2910189c98018c87245c1072ea82e61d5d4ac298
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-07-16 11:25:53 +0900

    Add moduledoc to Zenohex.Hello

--- lib/zenohex/hello.ex
+++ lib/zenohex/hello.ex
@@ -1,8 +1,25 @@
 defmodule Zenohex.Hello do
+  @moduledoc """
+  A zenoh Hello message.
+
+  ## Examples
+
+      iex> Zenohex.scout(:peer, Zenohex.Config.default(), 100)
+      {:ok,
+       [
+         %Zenohex.Hello{
+           locators: ["tcp/[fe80::dead:beaf:cafe:1234]:36319",
+            "tcp/10.0.123.45:36319"],
+           whatami: :peer,
+           zid: "de7815fc98e0bdbb69e84ff9653ee26"
+         }
+       ]}
+  """
+
   @type t :: %__MODULE__{
-          locators: String.t(),
+          locators: [String.t()],
           whatami: :peer | :router,
-          zid: String.t()
+          zid: Zenohex.Session.zid()
         }
 
   defstruct [
