1b6c5f49d6c87769dc45bb5ed9919981a1211a4a
Ryota Kinukawa <kinukawa@tombo-works.com>
2025-07-17 11:36:37 +0900

    Chore

--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -1,4 +1,6 @@
 defmodule Zenohex.Test.Support.TestHelper do
+  @moduledoc false
+
   @spec scouting_delay(binary(), non_neg_integer()) :: binary()
   def scouting_delay(config, delay) when is_integer(delay) and delay >= 0 do
     Zenohex.Config.update_in(config, ["scouting", "delay"], fn _ -> delay end)
