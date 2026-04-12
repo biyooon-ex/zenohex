50c609f7574d5827604c8e2d6504c84b5a192e6a
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-04-12 19:20:23 +0900

    refactor: Replace update_in with insert_json5 in test helper functions

--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -3,10 +3,16 @@ defmodule Zenohex.Test.Support.TestHelper do
 
   @spec scouting_delay(binary(), non_neg_integer()) :: binary()
   def scouting_delay(config, delay) when is_integer(delay) and delay >= 0 do
-    Zenohex.Config.update_in(config, ["scouting", "delay"], fn _ -> delay end)
+    {:ok, updated_config} =
+      Zenohex.Config.insert_json5(config, "scouting/delay", Integer.to_string(delay))
+
+    updated_config
   end
 
   def mode(config, mode) when mode in ["peer", "client"] do
-    Zenohex.Config.update_in(config, ["mode"], fn _ -> mode end)
+    {:ok, updated_config} =
+      Zenohex.Config.insert_json5(config, "mode", ~s("#{mode}"))
+
+    updated_config
   end
 end
