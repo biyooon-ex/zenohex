86ab0b9211900c6dc3297dfe5e541df52441dd0a
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-04-14 19:16:13 +0900

    refactor: update environment variable for NIF build mode in CI workflows and remove steps for debug

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,9 @@ import Config
 
 config :rustler_precompiled, :force_build, zenohex: true
 
+# CI optimization for GitHub Actions workflows:
+# skip Rustler compilation during `mix compile` if a cached NIF has been restored.
 config :zenohex, Zenohex.Nif,
-  skip_compilation?: System.get_env("SKIP_ZENOHEX_NIF_BUILD") in ["1", "true"]
+  skip_compilation?: System.get_env("GHA_SKIP_ZENOHEX_NIF_BUILD") in ["1", "true"]
 
 import_config "#{config_env()}.exs"
