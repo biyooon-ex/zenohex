d64694c4221de579bc3159932cc40191d764ba63
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-04-14 19:09:26 +0900

    feat: enhance caching for cargo in GHA workflows

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,7 @@ import Config
 
 config :rustler_precompiled, :force_build, zenohex: true
 
+config :zenohex, Zenohex.Nif,
+  skip_compilation?: System.get_env("SKIP_ZENOHEX_NIF_BUILD") in ["1", "true"]
+
 import_config "#{config_env()}.exs"
